e6fe6cc07b9f14855226e3223635c0142f3aa2e6
GizmoLD <122391569+GizmoLD@users.noreply.github.com>
2023-11-07 14:02:32 +0100

    terminado

--- app/src/main/java/com/example/mp08persistenciaendispositivosandroid/MainActivity.java
+++ app/src/main/java/com/example/mp08persistenciaendispositivosandroid/MainActivity.java
@@ -7,6 +7,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
     private Button button0;
@@ -27,14 +31,32 @@ public class MainActivity extends AppCompatActivity {
         editTextPhone = findViewById(R.id.editTextPhone);
         editTextTextEmailAddress = findViewById(R.id.editTextTextEmailAddress);
 
-        button0.setOnClickListener(new View.OnClickListener(){
-
+        button0.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (editTextTextNom!=null && editTextTextCognom!=null && editTextPhone!=null && editTextTextEmailAddress!=null){
+                    try {
+                        FileOutputStream fileOutputStream = openFileOutput("contactes.txt", MODE_APPEND);
+                        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream);
 
+                        String nom = editTextTextNom.getText().toString();
+                        String cognom = editTextTextCognom.getText().toString();
+                        String phone = editTextPhone.getText().toString();
+                        String email = editTextTextEmailAddress.getText().toString();
+
+                        outputStreamWriter.write(nom + ",");
+                        outputStreamWriter.write(cognom + ",");
+                        outputStreamWriter.write(phone + ",");
+                        outputStreamWriter.write( email + ";\n");
+
+                        outputStreamWriter.close();
+
+                        Toast.makeText(getApplicationContext(), "Datos guardados correctamente en " + getFilesDir() + "/contactes.txt", Toast.LENGTH_SHORT).show();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         });
     }
-}
+    }
